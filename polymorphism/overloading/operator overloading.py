36e918265ca8aad130ede40cc0aca95bf9afa715
suraj <job.surajmahamuni@gmail.com>
2026-03-29 23:55:15 -0700

    first

--- polymorphism/overloading/operator overloading.py
+++ polymorphism/overloading/operator overloading.py
@@ -25,3 +25,29 @@ print("Total no of pages: ",b1+b2)
 # //  --> _floordiv()__
 # % --> __mod()__
 # ** --> __pow()__
+
+
+######  * operator overloading #######3
+
+class Employee:
+    def __init__(self,name,salary_per_day):
+        self.name = name
+        self.salary_per_day = salary_per_day
+
+    def __mul__(self,other):
+        return self.salary_per_day*other.days
+
+
+class TimeSheet:
+    def __init__(self,name,days):
+        self.name = name
+        self.days = days
+
+
+e=Employee("Raj", 500)
+t=TimeSheet("Raj",50)
+
+print("Total earning: ", e*t)
+
+## we wrote __mul__() method in Employee class because in print we wrote e*t not t*e.
+## t*e will give error with this code. we need to write __mul__() method in TimeSheet class to make it work
