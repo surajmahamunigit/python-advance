e462b46290e804abfa386c776cfdf7238ca7e788
suraj <job.surajmahamuni@gmail.com>
2026-03-29 23:27:24 -0700

    first

---
+++ polymorphism/overloading/operator overloading.py
@@ -0,0 +1,17 @@
+from inheritance.MRO import B
+
+
+class Book:
+    def __init__(self,pages):
+        self.pages = pages
+
+    def __add__(self,other):
+        return self.pages+other.pages
+
+b1=Book(100)
+b2=Book(200)
+b3=Book(300)
+
+print("Total no of pages: ",b1+b2)
+
+# b1+b2+b3 not possible
