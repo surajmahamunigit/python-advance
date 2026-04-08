caa1bbd7bf2a16dd2b6f8d864f7e006c90f40f52
suraj <job.surajmahamuni@gmail.com>
2026-04-07 22:05:08 -0700

    first commit

---
+++ Tuple/tuple programs/find sum of each tuple in the list.py
@@ -0,0 +1,14 @@
+# write a program to find the sum of each tuple inside the list
+
+#user input
+list1=[(1,2),(3,4),(5,6)]
+
+#core logic
+
+#traverse the list and find sum of each tuple
+for t in list1:
+    print(sum(t))
+
+
+
+
