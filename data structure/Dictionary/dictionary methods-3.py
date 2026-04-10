05348b667ecc2f2f16230491468af2bbd8ca6d2d
suraj <job.surajmahamuni@gmail.com>
2026-04-09 23:38:50 -0700

    first commit

--- data structure/Dictionary/dictionary methods-3.py
+++ data structure/Dictionary/dictionary methods-3.py
@@ -20,3 +20,15 @@ print(dict1.pop(100)) # 'A'
 print(dict1.pop(800,"Key Not Present"))  # Key Not Present
 
 
+########## popitem() ############
+
+dict1={100:'A',200:'B',300:'C'}
+
+print(dict1.popitem())  # (300, 'C')
+print(dict1)  # {100: 'A', 200: 'B'}  --> C gone
+
+
+########### clear()  #############
+dict1={100:'A',200:'B',300:'C'}
+dict1.clear()
+print(dict1)  # {}
